79d9a44bd6daadccde62a71b5bc300790f6018a8
Manuel Martin <manmartin@mozilla.com>
2021-01-12 16:38:29 +0100

    Remove unused function

--- src/react-components/debug-panel/CollapsiblePanel.js
+++ src/react-components/debug-panel/CollapsiblePanel.js
@@ -15,19 +15,6 @@ function openLink(url) {
   win.focus();
 }
 
-function download(filename, text) {
-  const element = document.createElement("a");
-  element.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(text));
-  element.setAttribute("download", filename);
-
-  element.style.display = "none";
-  document.body.appendChild(element);
-
-  element.click();
-
-  document.body.removeChild(element);
-}
-
 export function CollapsiblePanel({
   title,
   border,
